5efae2a4d8e32ffe030e945aa30bc9054fc3636e
lwilli04 <leeloo767@gmail.com>
2025-12-02 19:53:14 -0600

    interactive elements

--- museum/js/script.js
+++ museum/js/script.js
@@ -8,5 +8,25 @@ $("#y-blank-2").toggleClass("less-margin");
 
 })
 
+$("#y-blank-1").on("click", function(){
+$("#y-blank-1").toggleClass("down-margin");
+
+})
+
+$("#y-blank-3").on("click", function(){
+$("#y-blank-3").toggleClass("down-margin");
+
+})
+
+$("#y-blank-4").on("click", function(){
+$("#y-blank-4").toggleClass("down-margin");
+
+})
+
+$("#w-blank-1").on("click", function(){
+$("#w-blank-1").toggleClass("down-margin");
+
+})
+
 
 

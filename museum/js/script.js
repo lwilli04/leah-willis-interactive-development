d65d79a42a796c80f5c4bf42e93a9b76fca04180
lwilli04 <leeloo767@gmail.com>
2025-12-04 15:41:44 -0600

    updates

--- museum/js/script.js
+++ museum/js/script.js
@@ -18,13 +18,48 @@ $("#y-blank-3").toggleClass("down-margin");
 
 })
 
-$("#y-blank-4").on("click", function(){
-$("#y-blank-4").toggleClass("down-margin");
+$("#w-blank-6").on("click", function(){
+$("#w-blank-6").toggleClass("down-margin");
 
 })
 
-$("#w-blank-1").on("click", function(){
-$("#w-blank-1").toggleClass("down-margin");
+$("#w-blank-4").on("click", function(){
+$("#w-blank-4").toggleClass("down-margin");
+
+})
+
+$("#w-blank-5").on("click", function(){
+$("#w-blank-5").toggleClass("down-margin");
+
+})
+
+$("#w-blank-7").on("click", function(){
+$("#w-blank-7").toggleClass("down-margin");
+
+})
+
+$("#w-blank-8").on("click", function(){
+$("#w-blank-8").toggleClass("down-margin");
+
+})
+
+$("#w-blank-9").on("click", function(){
+$("#w-blank-9").toggleClass("down-margin");
+
+})
+
+$("#w-blank-10").on("click", function(){
+$("#w-blank-10").toggleClass("down-margin");
+
+})
+
+$("#w-blank-11").on("click", function(){
+$("#w-blank-11").toggleClass("down-margin");
+
+})
+
+$("#w-blank-12").on("click", function(){
+$("#w-blank-12").toggleClass("down-margin");
 
 })
 
